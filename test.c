dacd766b8e805a796f40b815610bc123e8c262bd
Your Name <you@example.com>
2024-11-19 09:13:50 +0530

    whethwr num is positive

--- test.c
+++ test.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 int main(){
-	int x,i ;
+	int x ;
 	printf("enter the value of x \n");
 	scanf("%d",&x);
-	for(i=2;i<x;i++)
-		if(x%i==0){
-			printf("x is not a prime number\n");
-			break;
-	       	}
-		else{
-			printf("x is a prime number\n");
-		}
+	if(x>0){
+		printf("x is a positive number \n");
+	}
+	else{
+		printf("number entered is wrong \n");
+	}
 
 return 0;
 }
