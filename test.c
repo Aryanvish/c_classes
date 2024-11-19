e3d1af63568a705acc4f3a74a3086cc5c0ad4b14
Your Name <you@example.com>
2024-11-19 09:57:11 +0530

    six commit

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	int x ;
+	int x,a,b,c ;
 	printf("enter the value of x \n");
 	scanf("%d",&x);
 	if(x>0){
@@ -9,6 +9,12 @@ int main(){
 	else{
 		printf("number entered is wrong \n");
 	}
+a=x%10;
+	printf("a is %d \n",a);
+b=x%100;
+       printf("b is %d \n",b);
+c=x%1000;
+       printf("c is %d \n",c);
 
 return 0;
 }
