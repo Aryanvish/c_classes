0f4160c5397a5111a5d28e32d8f59505e8c4b512
Your Name <you@example.com>
2024-12-02 09:33:56 +0530

    searching element

--- array.c
+++ array.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	int i,x[5]={37,32,9,51,7},temp,j;
+	int i,x[5]={37,32,9,51,7},temp,j,k;
 	for(j=0;j<4;j++){
 
 	for(i=j;i<5;i++){
@@ -14,10 +14,25 @@ int main(){
 	for(i=0;i<5;i++){
 	printf("%d\t",x[i]);
 	}
+	int flag = 0;
+	printf("\n enter the value of k ");
+	scanf("%d",&k);
+	for(i=0;i<=5;i++){
+		if(k==x[i]){
+			printf("the element exists\n");
+			flag = 0;
+			break;
+		}
+		else{
+			flag = -1;
+			
+		}
+	}
 
-	
-                        
-
+	if(flag==-1)
+	{
+		printf("element does not exist\n");
+	}
 
 return 0;
 }
